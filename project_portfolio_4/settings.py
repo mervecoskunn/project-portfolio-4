7380254f6d5e21c23479a6f56bfaecce2f7c4b24
mervecoskunn <merve_coskun2013@hotmail.com>
2024-03-18 21:20:27 +0100

    added gunicorn for deploy heroku

--- project_portfolio_4/settings.py
+++ project_portfolio_4/settings.py
@@ -143,4 +143,5 @@ DEFAULT_FILE_STORAGE = 'project_portfolio_4.storage_backends.PublicMediaStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
+
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
